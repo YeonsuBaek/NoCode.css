45a327c4b9a0d9b5f457453cd7abc5b933df8053
yeonsuBeak <soothbugger@gmail.com>
2022-09-28 12:53:57 +0900

    💄 Reset styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,4 +4,35 @@
 
 <script></script>
 
-<style></style>
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  font-family: "Roboto", sans-serif;
+  color: #3c3d3f;
+}
+
+button {
+  border: 0;
+  outline: 0;
+  cursor: pointer;
+  background-color: transparent;
+}
+
+li {
+  list-style-type: none;
+}
+
+.container {
+  width: 100%;
+  padding: 0;
+  margin: 0 auto;
+  max-width: 704px;
+  background: pink;
+}
+</style>
